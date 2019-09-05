b0d1cf2a121685c0ece1e5b4801cf15c44b7a611
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-05 16:56:11 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,11 +1,11 @@
 require "spec_helper"
 
 def introduction(name)
-  puts "Hi, my name is #{name} ."
+  puts "Hi, my name is #{name}."
 end 
 
 
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}  ."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
